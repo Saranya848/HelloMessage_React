ad6079e3420e43bb1de5279ac1788d7a3157c032
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-04-25 00:57:29 +0530

    Adding Logo to message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-//import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 import React from "react";
 
-class App extends React.Component{
-  render(){
-    return(<div class="App"><h1>Hello from Bridgelabz</h1></div>)
-  }
+//class App extends React.Component{
+  function App() {
+    var name = "Saranya";
+  //}
+  //render() {
+    return (
+      <div class='App'>
+        <h1>"Hello {name} from Bridgelabz"</h1>
+        <img src={logo}
+          alt="The BridgeLabz logo: A Bridge to Employement through lab works"/>
+      </div>
+    );
+  //}
 }
 
 export default App;
