ff588e666d49e7415caee942d018899549b97f23
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-04-25 01:28:31 +0530

    On clicking logo then launches bridgelabz website

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import logo from './logo.jpg';
 import './App.css';
 import React from "react";
 
-//class App extends React.Component{
-  function App() {
-    var name = "Saranya";
-  //}
-  //render() {
+class App extends React.Component{
+  url = 'https://www.bridgelabz.com/'
+  constructor(){
+    super();
+    this.state = {
+      title: "Hello from Bridgelabz"
+    };
+  }
+ //onclick function
+ onClick=($event) =>{
+   console.log('save button is clicked',$event);
+   window.open(this.url,"_blank");
+ }
+  render() {
     return (
-      <div class='App'>
-        <h1>"Hello {name} from Bridgelabz"</h1>
-        <img src={logo}
-          alt="The BridgeLabz logo: A Bridge to Employement through lab works"/>
+      <div>
+        <h1>{this.state.title}</h1>
+        <img src={logo} onClick={this.onClick} alt="The BridgeLabz logo: A Bridge to Employement through lab works"
+        />
       </div>
     );
-  //}
+  }
 }
 
 export default App;
