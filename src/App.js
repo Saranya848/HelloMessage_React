458e98f8e7fd7363a7bdbb1e463c9b1cf2f7c489
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-04-25 01:40:00 +0530

    User input of the candidate

--- src/App.js
+++ src/App.js
@@ -7,20 +7,32 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      title: "Hello from Bridgelabz"
+      userName: ""
     };
   }
- //onclick function
- onClick=($event) =>{
-   console.log('save button is clicked',$event);
-   window.open(this.url,"_blank");
- }
+  // onClick function
+  onClick = (event) => {
+    console.log("save button is clicked ", { event });
+    window.open(this.url);
+  };
+  //onChange Event function
+  onChange = (event) => {
+    console.log("value is ", event.target.value);
+    // see the title using setState Method
+    this.setState({ userName: event.target.value });
+  };
   render() {
     return (
-      <div>
-        <h1>{this.state.title}</h1>
+      <div class = "App">
+        <div>
+          <h1>Hello {this.state.userName} from BridgeLabz</h1>
         <img src={logo} onClick={this.onClick} alt="The BridgeLabz logo: A Bridge to Employement through lab works"
         />
+        </div>
+
+        <div>
+          <input onChange={this.onChange} />
+        </div>
       </div>
     );
   }
